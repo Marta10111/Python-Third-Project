f7ebe90ce1ee0c7a0993f59ceb85b1fd289f08af
Marta <manta.101@hotmail.com>
2022-09-30 07:59:30 +0000

    Adding questions from 5 to 10 and if, else statements to them

--- run.py
+++ run.py
@@ -5,10 +5,9 @@ def start():
     """
     Game starts with message and options
     """
-    print("Welcome to the Horror Movie Quiz")
-
+    
     f = Figlet(font='poison')
-    print(colored(f.renderText('Horrorland'), 'red'))
+    print(colored(f.renderText('Horror Movie Quiz'), 'red'))
 
 start()
 
@@ -20,8 +19,10 @@ score = 0
 """
 Question one
 """
-print("What is the name of the camp where Jason Voorhees drowns in the Friday the 13th series?")
-answer_1 = input("a) Camp Eden Lake\nb) Camp Crystal Lake\nc) Camp Silver Lake\nd) Camp Diamond Lake\n")
+print("What is the name of the camp where Jason Voorhees drowns in the Friday\
+the 13th series?")
+answer_1 = input("a) Camp Eden Lake\nb) Camp Crystal Lake\nc) Camp Silver Lake\
+\nd) Camp Diamond Lake\n")
 if answer_1 == "b":
     print("Answer:", answer_1)
     score = score + 1
@@ -37,8 +38,9 @@ else:
 Question two
 """
 print("In which film did Annabelle the doll make her debut?")
-answer_2 = input("a) Annabelle\nb) The Nun\nc) The Conjuring\nd) Annabelle Comes Home\n")
-if answer_1 == "c":
+answer_2 = input("a) Annabelle\nb) The Nun\nc) The Conjuring\nd) Annabelle\
+Comes Home\n")
+if answer_2 == "c":
     print("Answer:", answer_2)
     score = score + 1
     print("You're right")
@@ -84,8 +86,10 @@ else:
 """
 Question five
 """
-print("In which horror movie does the phrase 'the power of Christ compels you', appears?")
-answer_5 = input("a) The Conjuring\nb) The Exorcist\nc) The Omen\nd) The Nun\n")
+print("In which horror movie does the phrase 'the power of Christ compels you'\
+, appears?")
+answer_5 = input("a) The Conjuring\nb) The Exorcist\nc) The Omen\nd) The Nun\
+\n")
 if answer_5 == "b":
     print("Answer:", answer_5)
     score = score + 1
@@ -96,6 +100,91 @@ else:
     print("You're wrong, the answer is b")
     print("score:", score)
     print("\n")
+
+"""
+Question six
+"""
+print("Who plays the role pf Jack Torrence in the movie 'The Shining'?")
+answer_6 = input("a) Danny Lloyd\nb) Danny DeVito\nc) Scatman Crothers\nd)\
+Jack Nicholson\n")
+if answer_6 == "d":
+    print("Answer:", answer_6)
+    score = score + 1
+    print("You're right")
+    print("score:", score)
+    print("\n")
+else:
+    print("You're wrong, the answer is d")
+    print("score:", score)
+    print("\n")
+
+"""
+Question seven
+"""
+print("Which of the following movies is not based on a Stephen King novel?")
+answer_7 = input("a) The Orphan\nb) IT\nc) Doctor Sleep\nd) Pet Sematary\n")
+if answer_7 == "a":
+    print("Answer:", answer_7)
+    score = score + 1
+    print("You're right")
+    print("score:", score)
+    print("\n")
+else:
+    print("You're wrong, the answer is a")
+    print("score:", score)
+    print("\n")
+
+"""
+Question eight
+"""
+print("What is the occupation of Hannibal Lecter in the movie 'The Silence of\
+the Lambs?")
+answer_8 = input("a) A Vet\nb) A Psychiatrist\nc) A Tailor\nd) A Teacher\n")
+if answer_8 == "b":
+    print("Answer:", answer_8)
+    score = score + 1
+    print("You're right")
+    print("score:", score)
+    print("\n")
+else:
+    print("You're wrong, the answer is b")
+    print("score:", score)
+    print("\n")
+
+"""
+Question nine
+"""
+print("Who rides around on a red tricycle?")
+answer_9 = input("a) Chucky\nb) IT\nc) Annabelle\nd) Jigsaw\n")
+if answer_9 == "d":
+    print("Answer:", answer_9)
+    score = score + 1
+    print("You're right")
+    print("score:", score)
+    print("\n")
+else:
+    print("You're wrong, the answer is d")
+    print("score:", score)
+    print("\n")
+
+"""
+Question ten
+"""
+print("Which colours is Freddy Krueger's jumper?")
+answer_10 = input("a)Red and Green\nb) Blue and Green\nc) Yellow and Red\n\
+d) Yellow and Green\n")
+if answer_10 == "a":
+    print("Answer:", answer_10)
+    score = score + 1
+    print("You're right")
+    print("score:", score)
+    print("\n")
+else:
+    print("You're wrong, the answer is a")
+    print("score:", score)
+    print("\n")
+
+
 
 
 

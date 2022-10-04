39c877dd5cd8c5c7f151c46483f08b66e3b191e8
Marta <manta.101@hotmail.com>
2022-10-04 13:19:48 +0000

    Adding play again function and beautifying file

--- run.py
+++ run.py
@@ -3,77 +3,72 @@ from colorama import Fore, Style
 colorama.init(autoreset=True)
 
 quiz_questions = [
-    {"question": "What is the name of the camp where Jason Voorhees drowns in the Friday\
-the 13th series?",
-    "answer": {"A": "Camp Eden Lake",
-               "B": "Camp Crystal Lake",
-               "C": "Camp Silver Lake",
-               "D": "Camp Diamond Lake"},
-    "correct_answer": "B"},
+    {"question": "What is the name of the camp where Jason Voorhees drowns\
+ in the Friday the 13th series?",
+     "answer": {"A": "Camp Eden Lake",
+                "B": "Camp Crystal Lake",
+                "C": "Camp Silver Lake",
+                "D": "Camp Diamond Lake"},
+     "correct_answer": "B"},
     {"question": "In which film did Annabelle the doll make her debut?",
-    "answer": {"A": "Annabelle",
-               "B": "The Nun",
-               "C": "The Conjuring",
-               "D": "Annabelle Comes Home"},
-    "correct_answer": "C"},
-     {"question": "What is the Exorcist's Demon's Name?",
-    "answer": {"A": "Pazuzu",
-               "B": "Azazel",
-               "C": "Mammon",
-               "D": "Lucifer"},
-    "correct_answer": "A"},
-     {"question": "What arms does Leatherface prefer?",
-    "answer": {"A": "Machete",
-               "B": "Kitchen knife",
-               "C": "The Axe",
-               "D": "Chainsaw"},
-    "correct_answer": "D"},
-     {"question": "In which horror movie does the phrase 'the power of Christ compels you'\
-, appears?",
-    "answer": {"A": "The Conjuring",
-               "B": "The Exorcist",
-               "C": "The Omen",
-               "D": "The Nun"},
-    "correct_answer": "B"},
-     {"question": "Who plays the role of Jack Torrence in the movie 'The Shining'",
-    "answer": {"A": "Danny Lloyd",
-               "B": "Danny DeVito",
-               "C": "Scatman Crothers",
-               "D": "Jack Nicholson"},
-    "correct_answer": "D"},
-    {"question": "Which of the following movies is not based on a Stephen King novel?",
-    "answer": {"A": "The Orphan",
-               "B": "IT",
-               "C": "Doctor Sleep",
-               "D": "Pet Sematary"},
-    "correct_answer": "A"},
-    {"question": "What is the occupation of Hannibal Lecter in the movie 'The Silence of\
-the Lambs?",
-    "answer": {"A": "A Vet",
-               "B": "A Psychiatrist",
-               "C": "A Tailor",
-               "D": "A Teacher"},
-    "correct_answer": "B"},
-     {"question": "Who rides around on a red tricycle?",
-    "answer": {"A": "Chucky",
-               "B": "IT",
-               "C": "Annabelle",
-               "D": "Jigsaw"},
-    "correct_answer": "D"},
-     {"question": "Which colours is Freddy Krueger's jumper?",
-    "answer": {"A": "Red and Green",
-               "B": "Green and Blue",
-               "C": "Blue and Red",
-               "D": "Red and Yellow"},
-    "correct_answer": "A"},
+     "answer": {"A": "Annabelle",
+                "B": "The Nun",
+                "C": "The Conjuring",
+                "D": "Annabelle Comes Home"},
+     "correct_answer": "C"},
+    {"question": "What is the Exorcist's Demon's Name?",
+     "answer": {"A": "Pazuzu",
+                "B": "Azazel",
+                "C": "Mammon",
+                "D": "Lucifer"},
+     "correct_answer": "A"},
+    {"question": "What arms does Leatherface prefer?",
+     "answer": {"A": "Machete",
+                "B": "Kitchen knife",
+                "C": "The Axe",
+                "D": "Chainsaw"},
+     "correct_answer": "D"},
+    {"question": "In which horror movie does the phrase 'the power of Christ\
+ compels you', appears?",
+     "answer": {"A": "The Conjuring",
+                "B": "The Exorcist",
+                "C": "The Omen",
+                "D": "The Nun"},
+     "correct_answer": "B"},
+    {"question": "Who plays the role of Jack Torrence in the movie\
+ 'The Shining'",
+     "answer": {"A": "Danny Lloyd",
+                "B": "Danny DeVito",
+                "C": "Scatman Crothers",
+                "D": "Jack Nicholson"},
+     "correct_answer": "D"},
+    {"question": "Which of the following movies is not based on a\
+ Stephen King novel?",
+     "answer": {"A": "The Orphan",
+                "B": "IT",
+                "C": "Doctor Sleep",
+                "D": "Pet Sematary"},
+     "correct_answer": "A"},
+    {"question": "What is the occupation of Hannibal Lecter in the movie 'The\
+ Silence of the Lambs?",
+     "answer": {"A": "A Vet",
+                "B": "A Psychiatrist",
+                "C": "A Tailor",
+                "D": "A Teacher"},
+     "correct_answer": "B"},
+    {"question": "Who rides around on a red tricycle?",
+     "answer": {"A": "Chucky",
+                "B": "IT",
+                "C": "Annabelle",
+                "D": "Jigsaw"},
+     "correct_answer": "D"},
+    {"question": "Which colours is Freddy Krueger's jumper?",
+     "answer": {"A": "Red and Green",
+                "B": "Green and Blue",
+                "C": "Blue and Red",
+                "D": "Red and Yellow"},
+     "correct_answer": "A"},
 ]
-
-def count_score(quiz_questions):
-    """
-    Counts correct and incorrect answers
-    """
-    score = 0
-    score  = score + 1
 
 
 def start_screen():
@@ -127,194 +122,45 @@ while start_quiz:
         print("Please type y or n\n")
 
 
+def run_game(quiz_questions):
+    """
+    Shows quiz questions and answers, add 1 score for correct answer or none\
+    for wrong answer
+    """
+    score = 0
+    for entry in quiz_questions:
+        user_answer = ''
+        while user_answer not in ['A', 'B', 'C', 'D']:
+            print(f"{Fore.BLUE}{Style.BRIGHT}{entry['question']}")
+
+            for key, value in entry['answer'].items():
+                print(f"\t{key}: {value}")
+
+            user_answer = input("Type your answer?\n")
+            user_answer = user_answer.upper()
+
+            if user_answer not in entry['answer']:
+                print("Only a, b, c or d are accepted\n")
+
+        if user_answer == entry['correct_answer']:
+            print(f"{Fore.GREEN}You're right\n")
+            score += 1
+        else:
+            print(f"{Fore.RED}You're wrong\n")
+
+    final_score(score)
 
 
-
-
-"""
-Defining score Variables
-"""
-score = 0
-
-def play_game():
-
-"""
-Question one
-"""
-
-print("What is the name of the camp where Jason Voorhees drowns in the Friday\
-the 13th series?")
-answer_1 = input("a) Camp Eden Lake\nb) Camp Crystal Lake\nc) Camp Silver Lake\
-\nd) Camp Diamond Lake\n")
-
-if answer_1 == "b":
-    print("Answer:", answer_1)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is b")
-    print("score:", score)
-    print("\n")
-
-"""
-Question two
-"""
-print()
-answer_2 = input("a) Annabelle\nb) The Nun\nc) The Conjuring\nd) Annabelle\
-Comes Home\n")
-if answer_2 == "c":
-    print("Answer:", answer_2)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is c")
-    print("score:", score)
-    print("\n")
-
-"""
-Quesation three
-"""
-print("What is the Exorcist's Demon's Name?")
-answer_3 = input("a) Pazuzu\nb) Azazel\nc) Mammon\nd) Lucifer\n")
-if answer_3 == "a":
-    print("Answer:", answer_3)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is a")
-    print("score:", score)
-    print("\n")
-
-"""
-Question four
-"""
-print("What arms does Leatherface prefer?")
-answer_4 = input("a) Machete\nb) Kitchen Knife\nc) The Axe\nd) Chainsaw\n")
-if answer_4 == "d":
-    print("Answer:", answer_4)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is d")
-    print("score:", score)
-    print("\n")
-
-"""
-Question five
-"""
-print("")
-answer_5 = input("a) The Conjuring\nb) The Exorcist\nc) The Omen\nd) The Nun\
-\n")
-if answer_5 == "b":
-    print("Answer:", answer_5)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is b")
-    print("score:", score)
-    print("\n")
-
-"""
-Question six
-print("?")
-answer_6 = input("a) Danny Lloyd\nb) Danny DeVito\nc) Scatman Crothers\nd)\
- Jack Nicholson\n")
-if answer_6 == "d":
-    print("Answer:", answer_6)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is d")
-    print("score:", score)
-    print("\n")
-
-"""
-Question seven
-"""
-print("")
-answer_7 = input("a) The Orphan\nb) IT\nc) Doctor Sleep\nd) Pet Sematary\n")
-if answer_7 == "a":
-    print("Answer:", answer_7)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is a")
-    print("score:", score)
-    print("\n")
-
-"""
-Question eight
-"""
-print("?")
-answer_8 = input("a) A Vet\nb) A Psychiatrist\nc) A Tailor\nd) A Teacher\n")
-if answer_8 == "b":
-    print("Answer:", answer_8)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is b")
-    print("score:", score)
-    print("\n")
-
-"""
-Question nine
-"""
-print("Who rides around on a red tricycle?")
-answer_9 = input("a) Chucky\nb) IT\nc) Annabelle\nd) Jigsaw\n")
-if answer_9 == "d":
-    print("Answer:", answer_9)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is d")
-    print("score:", score)
-    print("\n")
-
-"""
-Question ten
-"""
-print("Which colours is Freddy Krueger's jumper?")
-answer_10 = input("a) Red and Green\nb) Blue and Green\nc) Yellow and Red\n\
-d) Yellow and Green\n")
-if answer_10 == "a":
-    print("Answer:", answer_10)
-    score = score + 1
-    print("You're right")
-    print("score:", score)
-    print("\n")
-else:
-    print("You're wrong, the answer is a")
-    print("score:", score)
-    print("\n")
-
-
-""" 
-Display final score at the end of quiz
-"""
-if score <= 5:
-    print(f"You know nothing {NAME}! Your final score is {score} out of\
+def final_score(score):
+    """ 
+    Display final score at the end of quiz
+    """
+    if score <= 5:
+        print(f"You know nothing {NAME}! Your final score is {score} out of\
  10. Better luck next time.")
-    print("Take the quiz again to see if you can improve your score.\n")
-elif score > 5:
-    print(f"Well done {NAME}! Your final score is {score} out of\
+        print("Take the quiz again to see if you can improve your score.\n")
+    elif score > 5:
+        print(f"Well done {NAME}! Your final score is {score} out of\
  10. You are a true Horror Movie fan.")
 
 
@@ -324,25 +170,28 @@ def play_again():
     start, if not quiz ends
     """
 
+    restart_quiz = True
 
-restart_quiz = True
+    while restart_quiz:
+        response = input("Do you want to play again? y/n\n")
+        response = response.upper()
 
-while restart_quiz:
-    response = input("Do you want to play again? y/n\n")
-    response = response.upper()
-
-    if response == "Y":
-        return True
-    elif response == "N":
-        return False
-    else:
-        print("Please type 'Y' if you want start again, or 'N'\
+        if response == "Y":
+            return True
+        elif response == "N":
+            return False
+        else:
+            print("Please type 'Y' if you want start again, or 'N'\
             if you want to quit \n")
 
 
-while play_again():
-    play_game()
+start_screen()
+run_game(quiz_questions)
 
+
+while play_again():
+    start_screen()
+    run_game(quiz_questions)
 else:
     print("The End\nThank you for playing")
     print("click the 'RUN PROGRAM' button to reset the quiz")
